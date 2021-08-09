3cdf4a4e5c75180271da736c3631ea5ebf03bee8
Benedikt Walch <benx.bw@gmail.com>
2021-08-09 16:43:39 +0200

    Logic to display all habbits that are stored in local storage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,28 @@ import "./Home.css";
 import "../components/colors.css";
 
 export default function Home() {
+  function RenderHabbits() {
+    const habbits = JSON.parse(localStorage.getItem("habbits"));
+
+    if (habbits !== null) {
+      const renderedhabbits = habbits.map((habbit) => {
+        return <button>{habbit}</button>;
+      });
+      return renderedhabbits;
+    } else {
+      return (
+        <h2>
+          There are no habbits stored. You can find the Create New section in
+          the Menu to create your first habbit.
+        </h2>
+      );
+    }
+  }
   return (
     <>
       <Header />
       <main className="main-home">
-        <h2>Home</h2>
+        <RenderHabbits />
       </main>
     </>
   );
