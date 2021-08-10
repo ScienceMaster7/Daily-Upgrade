e6177f5c2bf3e824e1ae17c28f6ce115b934b260
Benedikt Walch <benx.bw@gmail.com>
2021-08-10 11:09:30 +0200

    Footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
+import "./Footer.css";
+import "./colors.css";
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="footer">
       <Link to="/home" className="link">
         <FontAwesomeIcon icon={faHome} className="homeIcon" />
       </Link>
