7f97dd5842040a06825e0ad9e7da94accf8ce28b
Benedikt Walch <benx.bw@gmail.com>
2021-08-11 16:06:18 +0200

    Create and Style Input field for hours and minutes

--- src/pages/Habit.js
+++ src/pages/Habit.js
@@ -4,20 +4,58 @@ import "./Habit.css";
 
 export default function Habit() {
   const { habbit } = useParams();
+
+  // function log(event) {
+  //   const value = event.target.value;
+  //   console.log(value);
+  // }
   return (
     <>
       <main className="Habit__main">
         <h2 className="Habit__title">{habbit}</h2>
         <form className="Habit__form">
-          <input
-            type="text"
-            className="Habit__input"
-            name="timeInput"
-            id="timeInput"
-            placeholder="Enter your time here"
-            autoComplete="off"
-            required={true}
-          />
+          <section className="Habit__time">
+            <div>
+              <label className="Habit__label" htmlFor="hours">
+                Hours:
+              </label>
+              <input
+                type="number"
+                className="Habit__input"
+                name="hours"
+                id="hours"
+                autoComplete="off"
+                min="0"
+                max="20"
+                placeholder="3"
+                required={true}
+              />
+            </div>
+            <div>
+              <label className="Habit__label" htmlFor="minutes">
+                Minutes:
+              </label>
+              <input
+                type="number"
+                className="Habit__input"
+                name="minutes"
+                id="minutes"
+                autoComplete="off"
+                min="0"
+                max="59"
+                placeholder="20"
+                required={true}
+              />
+            </div>
+          </section>
+          {/* <input
+            onChange={log}
+            type="range"
+            name="hours"
+            id="hours"
+            min="0"
+            max="23"
+          /> */}
           <button type="submit" className="Habit__button">
             Submit
           </button>
