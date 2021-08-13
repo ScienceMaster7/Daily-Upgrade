49dbaefc6e510ab6cfe8cacacf37d9cadfa5e552
Benedikt Walch <benx.bw@gmail.com>
2021-08-13 17:12:51 +0200

    Positioned progress bar and button text

--- src/components/HabitList.js
+++ src/components/HabitList.js
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function HabitList() {
   const habits = JSON.parse(localStorage.getItem("habits"));
 
   if (habits !== null) {
     const renderedHabits = habits.map((habit, index) => {
+      let percentage = 66;
       return (
         <Link key={index} to={`/${habit.name}`} className="Home__link">
           <button className="Home__button">
-            <p>{habit.name}</p>
-            <p>Level: {habit.level}</p>
+            <CircularProgressbar
+              value={percentage}
+              className="Home__progressbar"
+            />
+            <div className="Home__button__text">
+              <p>{habit.name}</p>
+              <p>Level: {habit.level}</p>
+            </div>
           </button>
         </Link>
       );
