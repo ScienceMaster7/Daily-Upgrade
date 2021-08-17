ef2b6bee4b4dece322087951ea6a9bcab438699d
Benedikt Walch <benx.bw@gmail.com>
2021-08-17 16:12:43 +0200

    Uninstall react-calendar

--- src/pages/ProgressLogs.js
+++ src/pages/ProgressLogs.js
@@ -1,18 +1,19 @@
 import Footer from "../components/Footer";
-import Calendar from "react-calendar";
+// import Calendar from "react-calendar";
 // import StreakItems from "../components/StreakItems";
 import "./ProgressLogs.css";
 
 export default function ProgressLogs() {
   const habits = JSON.parse(localStorage.getItem("habits"));
   const dateAndTime = habits.map((habit, index) => {
+    console.log(habit.dateTracker[0].date);
     return (
       <section key={index} className="card">
         <h2 className="card__habitName">{habit.name}</h2>
         {/* <div className="card__streak">
           <StreakItems dates={habit.dateTracker} />
         </div> */}
-        <Calendar />
+        {/* <Calendar minDate={habit.dateTracker[0].date} /> */}
       </section>
     );
   });
