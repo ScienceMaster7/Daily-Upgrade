8a05f3769f4a4f6377882f18bd2a1ebf29ced080
Benedikt Walch <benx.bw@gmail.com>
2021-08-23 13:29:09 +0200

    Implement Logic to display last dayof the month

--- src/components/StreakItems.js
+++ src/components/StreakItems.js
@@ -1,6 +1,17 @@
-export default function StreakItems({ days }) {
+export default function StreakItems({ days, month, today }) {
   let renderedDays = [];
-  for (let i = 1; i <= days[days.length - 1]; i++) {
+  let lastDay;
+
+  if (month === today[1]) {
+    lastDay = days[days.length - 1];
+  } else if (month === 1) {
+    lastDay = 28;
+  } else if (month % 2 === 0) {
+    lastDay = 31;
+  } else {
+    lastDay = 30;
+  }
+  for (let i = 1; i <= lastDay; i++) {
     const renderedDay = (
       <div
         key={i}
