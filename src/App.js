93336c757428de1999047f4e1763743876d3d6fa
Benedikt Walch <benx.bw@gmail.com>
2021-08-25 15:49:15 +0200

    Create and link accomplishments page and current Challenges page.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import ProgressLogs from "./pages/ProgressLogs";
 import AppFacts from "./pages/AppFacts";
 import Challenges from "./pages/Challenges";
 import NewChallenges from "./pages/NewChallenges";
+import CurrentChallenges from "./pages/CurrentChallenges";
+import Accomplishments from "./pages/Accomplishments";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 function App() {
@@ -47,6 +49,12 @@ function App() {
           </Route>
           <Route path="/new-challenge">
             <NewChallenges />
+          </Route>
+          <Route path="/current-challenge">
+            <CurrentChallenges />
+          </Route>
+          <Route path="/accomplishments">
+            <Accomplishments />
           </Route>
           <Route path="/:habitname">
             <Habit />
---
+++ src/pages/Accomplishments.js
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import "./Accomplishments.css";
+
+export default function Accomplishments() {
+  return (
+    <>
+      <main className="Accomplishments__main">
+        <p className="Accomplishments__text">No Accomplishments yet.</p>
+      </main>
+      <Footer />
+    </>
+  );
+}
