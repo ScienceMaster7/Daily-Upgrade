137d9d4004986491aaaa1c93562138ef7f921da1
Benedikt Walch <benx.bw@gmail.com>
2021-08-31 07:50:18 +0200

    Fix bug in date tracker

--- src/services/updateDateTracker.js
+++ src/services/updateDateTracker.js
@@ -2,17 +2,17 @@ import todaysDate from "./todaysDate";
 
 export default function updateDateTracker(datesArray, spentTime) {
   const today = todaysDate();
-
-  if (datesArray[datesArray.length - 1].date !== today) {
-    datesArray.push({
-      date: today,
-      timeToday: spentTime,
-    });
-  } else {
+  if (datesArray[datesArray.length - 1].date.join(".") === today.join(".")) {
+    console.log("this works");
     datesArray[datesArray.length - 1].date =
       datesArray[datesArray.length - 1].date;
     datesArray[datesArray.length - 1].timeToday =
       datesArray[datesArray.length - 1].timeToday + spentTime;
+  } else {
+    datesArray.push({
+      date: today,
+      timeToday: spentTime,
+    });
   }
   return datesArray;
 }
